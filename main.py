077753840aa645f66774aaf5f30846505cc47f6b
nam tran trong <nam.tran-trong@ubisoft.com>
2024-06-15 21:58:24 +0700

    update

--- main.py
+++ main.py
@@ -8,24 +8,24 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 if "__main__" == __name__:
 
-    # # extract video
-    # start_time = time.time()
-    # video_path = 'video.mp4'
-    # output_folder = 'Assets/Extract'
-    # second_interval = 1
-    # extractor = FrameExtractor(video_path, output_folder, second_interval)
-    # extractor.extract_frames()
-    # end_time = time.time()
-    # execution_time = end_time - start_time
-    # print(f"Thời gian thực hiện extract video: {execution_time} giây")
+    # extract video
+    start_time = time.time()
+    video_path = 'video.mp4'
+    output_folder = 'Assets/Extract'
+    second_interval = 1
+    extractor = FrameExtractor(video_path, output_folder, second_interval)
+    extractor.extract_frames()
+    end_time = time.time()
+    execution_time = end_time - start_time
+    print(f"Thời gian thực hiện extract video: {execution_time} giây")
 
     # face detector
-    # start_time = time.time()
-    # face_extractor = FaceExtractor()
-    # face_extractor.process_images()
-    # end_time = time.time()
-    # execution_time = end_time - start_time
-    # print(f"Thời gian thực hiện face detector: {execution_time} giây")
+    start_time = time.time()
+    face_extractor = FaceExtractor()
+    face_extractor.process_images()
+    end_time = time.time()
+    execution_time = end_time - start_time
+    print(f"Thời gian thực hiện face detector: {execution_time} giây")
 
     # matching
     start_time = time.time()
